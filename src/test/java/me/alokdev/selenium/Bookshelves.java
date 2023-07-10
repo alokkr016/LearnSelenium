ae541633a93d83f5a7339cc5a8499ae72b6e36c9
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2023-07-10 12:57:20 +0530

    Added extent report

--- src/test/java/me/alokdev/selenium/Bookshelves.java
+++ src/test/java/me/alokdev/selenium/Bookshelves.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -18,10 +17,12 @@ public class Bookshelves {
 	public String baseUrl = "https://www.urbanladder.com/";
 	public WebDriver driver;
 	public JavascriptExecutor js;
+	ReportGenerator report;
 
 	@BeforeTest
-	public void launchWebsite() throws InterruptedException {
+	public void launchThePage() throws InterruptedException {
 		driver = DriverSetup.getDriver("Chrome");
+		report = new ReportGenerator();
 		// Launch and maximize
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
@@ -30,42 +31,60 @@ public class Bookshelves {
 
 	@Test(priority = 0)
 	public void scrollAndClick() throws InterruptedException {
+		report.testName("Click on bookshelves");
+		report.logsInfo("Scrolling down to make element visible");
 		// Scroll down
 		js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,500)");
 
+		report.logsInfo("Clicking on bookshelves");
 		// click on bookshelves
 		driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div[3]/a[5]")).click();
 		Thread.sleep(5000);
 
 		// To close the pop up
+		report.logsInfo("Close the pop-up");
 		driver.findElement(By.xpath("//*[@id=\"authentication_popup\"]/div[1]/div/div[2]/a[1]")).click();
 		Thread.sleep(3000);
+		try {
+			report.flushReport();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Test(priority = 1)
-	public void setThePriceFilter() throws InterruptedException {
+	public void setRequiredFilter() throws InterruptedException {
+		report.testName(" Displaying the name & price of first 3 Bookshelves");
+		
+		report.logsInfo("Clicking on price filter");
 		// Clicking on price caret
 		driver.findElement(By.xpath("//*[@id=\"filters-form\"]/div[1]/div/div/ul/li[1]/div[1]/div")).click();
 		Thread.sleep(1000);
 		WebElement frame = driver.findElement(By.xpath("//div[@class='noUi-handle noUi-handle-upper']"));
 		Actions action = new Actions(driver);
+		
+		report.logsInfo("Applying the price filter");
 		// set on price range
 		action.dragAndDropBy(frame, -244, 0).perform();
-
+		Thread.sleep(3000);
+		
+		report.logsInfo("Applying storage filter");
+		// Click on storage caret
+		driver.findElement(By.xpath("//*[@id=\"filters-form\"]/div[1]/div/div/ul/li[2]/div[1]/div")).click();
+		Thread.sleep(3000);
+		
+		report.logsInfo("Applyting open filter");
+		// select open
+		driver.findElement(By.xpath("//input[@id='filters_storage_type_Open']")).click();
 		Thread.sleep(3000);
 	}
 
 	@Test(priority = 2)
-	public void selectTheStorageType() throws InterruptedException {
-		// select storage type
-		// Click on storage caret
-		driver.findElement(By.xpath("//*[@id=\"filters-form\"]/div[1]/div/div/ul/li[2]/div[1]/div")).click();
-		Thread.sleep(3000);
-
-		// select open
-		driver.findElement(By.xpath("//input[@id='filters_storage_type_Open']")).click();
-		Thread.sleep(3000);
+	public void printTheDetails() throws InterruptedException {
+		report.testName("Printing the details after applying filter");
+		
 		// list of bookshelves
 		List<WebElement> bookshelves = driver
 				.findElements(By.xpath("//*[@class='product-title product-title-sofa-mattresses']/span"));
@@ -73,6 +92,7 @@ public class Bookshelves {
 		// list of price
 		List<WebElement> prices = driver.findElements(By.xpath("//*[@class='price-number']/span"));
 
+		report.logsInfo("Printing the bookshelves details after applying filter");
 		// printing 3 bookshelves details
 		for (int i = 0; i < 3; i++) {
 
@@ -83,110 +103,116 @@ public class Bookshelves {
 			System.out.println(price);
 
 		}
-
+		
+		report.logsInfo("Scrolling to navbar");
 		// navbar
 		WebElement navbar = driver.findElement(By.id("topnav_wrapper"));
 		js.executeScript("arguments[0].scrollIntoView();", navbar);
 		Thread.sleep(3000);
 
+		report.logsInfo("Click on giftcard");
 		// click on Gift card
 		driver.findElement(By.xpath("//*[@id=\"header\"]/section/div/ul[2]/li[3]/a")).click();
 
 	}
 
-	@Test(priority = 3)
-	public void chooseBirthdayCard() throws InterruptedException {
-		js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,500)");
-		Thread.sleep(3000);
-
-		// Click on B'day card
-		driver.findElement(By.xpath("//*[@id=\"app-container\"]/div/main/section/section[1]/ul/li[3]")).click();
-		Thread.sleep(3000);
-
-		// Entering amount
-		driver.findElement(By.xpath("//input[@id='ip_2251506436']")).sendKeys("1000");
-		Thread.sleep(2000);
-
-		// Select month
-		Select month = new Select(driver.findElement(
-				By.xpath("//*[@id=\"app-container\"]/div/main/section/section[2]/div/section[2]/div[4]/select[1]")));
-		month.selectByValue("7/2023");
-
-		//select date
-		Select date = new Select(driver.findElement(
-				By.xpath("//*[@id=\'app-container\']/div/main/section/section[2]/div/section[2]/div[4]/select[2]")));
-		date.selectByVisibleText("18");
-
-		//Click on next button
-		driver.findElement(By.xpath("//button[@class='_1IFIb _1fVSi action-button _1gIUf _1XfDi']")).click();
-
-	}
+//	@Test(priority = 3)
+//	public void chooseBirthdayCard() throws InterruptedException {
+//		js = (JavascriptExecutor) driver;
+//		js.executeScript("window.scrollBy(0,500)");
+//		Thread.sleep(3000);
+//
+//		// Click on B'day card
+//		driver.findElement(By.xpath("//*[@id=\"app-container\"]/div/main/section/section[1]/ul/li[3]")).click();
+//		Thread.sleep(3000);
+//
+//		// Entering amount
+//		driver.findElement(By.xpath("//input[@id='ip_2251506436']")).sendKeys("1000");
+//		Thread.sleep(2000);
+//
+//		// Select month
+//		Select month = new Select(driver.findElement(
+//				By.xpath("//*[@id=\"app-container\"]/div/main/section/section[2]/div/section[2]/div[4]/select[1]")));
+//		month.selectByValue("7/2023");
+//
+//		//select date
+//		Select date = new Select(driver.findElement(
+//				By.xpath("//*[@id=\'app-container\']/div/main/section/section[2]/div/section[2]/div[4]/select[2]")));
+//		date.selectByVisibleText("18");
+//
+//		//Click on next button
+//		driver.findElement(By.xpath("//button[@class='_1IFIb _1fVSi action-button _1gIUf _1XfDi']")).click();
+//
+//	}
 //
 //	@Test(priority = 4)
 //	public void fillDetails() throws InterruptedException, IOException {
-//		String path = "src/main/java/com/cognizant/Book.xlsx";
-//		// Recipient
+//		//Excel file path
+//		String path = "src/main/java/com/cognizant/DetailsSheet.xlsx";
+//		// Filling Recipient details
 //		String[] recipientDetails = ReadExcel.getData(path, 0).split(" ");
-//
 //		driver.findElement(By.id("ip_4036288348")).sendKeys(recipientDetails[0]);
 //		driver.findElement(By.id("ip_137656023")).sendKeys(recipientDetails[1]);
 //		driver.findElement(By.id("ip_3177473671")).sendKeys(recipientDetails[2]);
 //
-//		// Sender
+//		// Filling sender details
 //		String[] senderDetails = ReadExcel.getData(path, 1).split(" ");
 //		driver.findElement(By.id("ip_1082986083")).sendKeys(senderDetails[0]);
 //		driver.findElement(By.id("ip_4081352456")).sendKeys(senderDetails[1]);
 //		driver.findElement(By.id("ip_2121573464")).sendKeys(senderDetails[2]);
 //		driver.findElement(By.id("ip_2194351474")).sendKeys(senderDetails[3]);
 //		driver.findElement(By.id("ip_567727260")).sendKeys(senderDetails[4]);
-//
 //		Thread.sleep(3000);
 //
-//		String error = driver.findElement(By.xpath("//div[@class='_1HVuH']")).getText();
+//		//Getting Pincode error message
+//		String pincodeError = driver.findElement(By.xpath("//div[@class='_1HVuH']")).getText();
+//		System.out.println(pincodeError);
 //
-//		System.out.println(error);
-//
+//		//correcting details
 //		driver.findElement(By.id("ip_567727260")).clear();
 //		Thread.sleep(3000);
 //		driver.findElement(By.id("ip_567727260")).sendKeys("560005");
-//
 //		Thread.sleep(3000);
-//
+//		
+//		//clicking on confirm button
 //		driver.findElement(By.xpath("//button[@class='_3Hxyv _1fVSi action-button _1gIUf _1XfDi']")).click();
 //
-//		String error2 = driver.findElement(By.xpath("//input[@name='recipient_mobile_number']")).getAttribute("title");
-//		System.out.println(error2);
+//		//Getting mobile number error message
+//		String mobileNoError = driver.findElement(By.xpath("//input[@name='recipient_mobile_number']")).getAttribute("title");
+//		System.out.println(mobileNoError);
 //
+//		//correcting details
 //		driver.findElement(By.id("ip_3177473671")).clear();
 //		driver.findElement(By.id("ip_3177473671")).sendKeys("8985425562");
 //		Thread.sleep(3000);
+//		
+//		//clicking on confirm button
 //		driver.findElement(By.xpath("//button[@class='_3Hxyv _1fVSi action-button _1gIUf _1XfDi']")).click();
-//
 //		Thread.sleep(3000);
-//
+//		
+//		//navigating back
 //		driver.navigate().back();
-//
 //		Thread.sleep(3000);
 //
 //	}
 //
 //	@Test(priority = 5)
 //	public void retrieveAllSubmenuItemsAtHome() throws InterruptedException {
-//
+//		//clicking on brand toggle
 //		driver.findElement(By.xpath("//li[@data-group='brand']")).click();
 //
+//		//Filtering by At-home
 //		driver.findElement(By.xpath("//input[@id='filters_brand_name_By_home']")).click();
-//
 //		Thread.sleep(3000);
-//
-//		WebElement home = driver
+//		
+//		//Fetching all data of bookshelves
+//		WebElement bookshelvesGrid = driver
 //				.findElement(By.xpath("//ul[@class='productlist withdivider clearfix bookshelves productgrid']"));
 //
-//		List<WebElement> beingHomeProduct = home
+//		List<WebElement> beingHomeProduct = bookshelvesGrid
 //				.findElements(By.xpath("//div[@class='product-title product-title-sofa-mattresses']/span"));
 //
-//		List<WebElement> beingHomePrice = home.findElements(By.xpath("//div[@class='price-number']/span"));
+//		List<WebElement> beingHomePrice = bookshelvesGrid.findElements(By.xpath("//div[@class='price-number']/span"));
 //
 //		System.out.println("Total number of products" + beingHomeProduct.size());
 //
@@ -202,7 +228,7 @@ public class Bookshelves {
 //
 //		}
 //	}
-//
+
 //	@AfterTest
 //	public void terminateBrowser() {
 //		driver.close();
